c9d1aaa6aac6507dd4da7bf46acd5830e226c09b
NavigationHazard <eiselt.p@googlemail.com>
2022-05-06 07:17:35 +0200

    noreferer

--- components/footer.tsx
+++ components/footer.tsx
@@ -11,7 +11,7 @@ const Footer: React.FC = () => {
           
             <div className=" mx-auto text-center bg-emerald-200/25 justify-center items-center flex flex-col-2 space-x-4 p-10">
            
-              <a href="https://github.com/NavigationHazard/" target='_blank' ><Image src='/assets/GitHub-Mark-Light-64px.png' alt='github' height={64} width={64} /></a>
+              <a href="https://github.com/NavigationHazard/" target='_blank' rel="noreferrer" ><Image src='/assets/GitHub-Mark-Light-64px.png' alt='github' height={64} width={64} /></a>
             </div>
             <div className=" mx-auto text-center bg-emerald-200/25 justify-center items-center flex   space-x-4 pb-10   border-black">
                 <p className="text-slate-200 text-[0.5em] tracking-tight font-mono">&copy; Copyright {year}, NavigationHazard</p>
